cb8d44e1470ab0ffdb0a2b45a3f6631ebbf1563a
Nurdilla <nurdilladilla209@gmail.com>
2021-10-18 12:08:28 +0700

    first commit

---
+++ JavaMethod.java
@@ -0,0 +1,14 @@
+public class JavaMethod{
+
+    public static void main(String[] args){
+        human("Nurdilla", 20);
+    }
+
+    
+    static void human(String nama, int umur){
+        System.out.println("nama :" + nama + ", umur ;" + umur);
+    }
+    
+        
+    }
+
